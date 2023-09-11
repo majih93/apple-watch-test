934dad748b11ed4f5234a0a2ac4eb9d64a108123
James Ma <t90laser93@gmail.com>
2023-09-11 23:06:11 +0900

    Initial Commit

---
+++ watch-test Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  watch-test Watch App
+//
+//  Created by James Ma on 2023/09/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watch-test Watch App/watch_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  watch_testApp.swift
+//  watch-test Watch App
+//
+//  Created by James Ma on 2023/09/11.
+//
+
+import SwiftUI
+
+@main
+struct watch_test_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
